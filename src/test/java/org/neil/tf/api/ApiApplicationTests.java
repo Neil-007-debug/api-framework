c5621f5249bfc5f766920c8c563fb7e7b784c328
Neil <neilhon@163.com>
2020-09-25 07:51:20 +0800

    first commit

---
+++ src/test/java/org/neil/tf/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.neil.tf.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
